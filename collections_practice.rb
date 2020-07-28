a63ff1311e35d762b52181e76633d639182fc801
Anya Brun <tsbrun@icloud.com>
2020-07-28 18:43:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  second_element = array[1]
+  third_element = array[2]
+  array[1] = third_element
+  array[2] = second_element
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  element_x = array[index]
+  element_y = array[destination_index]
+  array[index] = element_y
+  array[destination_index] = element_x
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map { |word| word[2] = "$" }
+  array
+end
+
+def find_a(array)
+  array.select { |word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject { |sum, number| sum + number}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index!= 1
+      element << "s"
+    else
+      element = array[index] ;
+    end
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,12 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'allow you to specify the index of the element you would like to move to a new index' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
